4a98535cb5b0c44e97c05a19776dacd4279c2800
Enes Ceylan <enesfurkanceylan@hotmail.com>
2024-10-07 23:04:06 +0300

    Setup the project.

---
+++ app/api/route.ts
@@ -0,0 +1,17 @@
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db('sample_airbnb'); 
+    const collection = db.collection('listingsAndReviews'); 
+
+    const data = await collection.find({}).limit(10).toArray();
+
+    return NextResponse.json({ success: true, data });
+  } catch (error) {
+    console.error('MongoDB Hatası:', error);
+    return NextResponse.json({ success: false, message: 'Veri çekilirken hata oluştu.' }, { status: 500 });
+  }
+}
