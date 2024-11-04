343cf44fb9d3904be99dc31cac36e42553c5a7ff
Salih Akyüzoğlu <sakyuzogluh@gmail.com>
2024-11-05 01:02:53 +0300

    The cookie control system has been added for security.

--- app/api/auth/check-token/route.ts
+++ app/api/auth/check-token/route.ts
@@ -1,7 +1,30 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import getUserEmail from "@/lib/helpers/getUserEmail";
+
+const SECRET_KEY = process.env.JWT_SECRET_KEY as string;
 
 export async function GET() {
-    const token = cookies().get("session-token");
-    return NextResponse.json({ isLoggedIn: !!token });
+  const token = cookies().get("session-token");
+
+  if (!token) {
+    return NextResponse.json({ isLoggedIn: false });
+  }
+
+  try {
+    const { email } = jwt.verify(token.value, SECRET_KEY) as { email: string };
+
+    const verifyEmail = await getUserEmail(email);
+    if (!verifyEmail) {
+      return NextResponse.json({ isLoggedIn: false });
+    }
+
+    return NextResponse.json({ isLoggedIn: true });
+  } catch (e) {
+    return NextResponse.json({
+      isLoggedIn: false,
+      error: e || "An unknown error occurred.",
+    });
+  }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,35 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import { jwtVerify } from "jose";
 
-export function middleware(request: NextRequest) {
-	const token = request.cookies.get("session-token")?.value;
+const JWT_KEY = process.env.JWT_SECRET_KEY;
+const SECRET_KEY = new TextEncoder().encode(JWT_KEY);
 
-	if (!token) {
-		return NextResponse.redirect(new URL("/auth/login", request.url));
-	}
+async function verifyJWT(token: string) {
+  try {
+    const { payload } = await jwtVerify(token, SECRET_KEY);
+    return payload;
+  } catch (e) {
+    throw new Error("Invalid token");
+  }
+}
 
-	return NextResponse.next();
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("session-token")?.value;
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  try {
+    await verifyJWT(token);
+  } catch (e) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-	matcher: ["/dashboard/:path*", "/profile/:path*"],
+  matcher: ["/dashboard/:path*", "/profile/:path*"],
 };
